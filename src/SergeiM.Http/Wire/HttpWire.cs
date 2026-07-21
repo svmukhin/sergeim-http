2c33d19bf41292648bea1142674ef835ee52b22b
Sergei Mukhin <sergei_mukhin@outlook.com>
2026-07-21 22:55:10 +0300

    refactor: extract HTTP request building into WireHelper

--- src/SergeiM.Http/Wire/HttpWire.cs
+++ src/SergeiM.Http/Wire/HttpWire.cs
@@ -1,8 +1,6 @@
 // SPDX-FileCopyrightText: Copyright (c) [2025-2026] [Sergei Mukhin]
 // SPDX-License-Identifier: MIT
 
-using System.Net.Http.Headers;
-
 namespace SergeiM.Http.Wire;
 
 /// <summary>
@@ -29,26 +27,10 @@ public class HttpWire : IWire
     }
 
     /// <inheritdoc/>
+    /// <inheritdoc/>
     public async Task<HttpResponseMessage> SendAsync(string method, string uri, Dictionary<string, string> headers, string? body = null)
     {
-        using var request = new HttpRequestMessage(new HttpMethod(method), uri);
-        string? contentType = null;
-        var headersCopy = new Dictionary<string, string>(headers);
-        if (headersCopy.ContainsKey("Content-Type"))
-        {
-            contentType = headersCopy["Content-Type"];
-            _ = headersCopy.Remove("Content-Type");
-        }
-        foreach (KeyValuePair<string, string> header in headersCopy)
-        {
-            _ = request.Headers.TryAddWithoutValidation(header.Key, header.Value);
-        }
-        if (body != null)
-        {
-            request.Content = contentType != null
-                ? new StringContent(body, System.Text.Encoding.UTF8, MediaTypeHeaderValue.Parse(contentType))
-                : new StringContent(body);
-        }
+        using var request = WireHelper.BuildRequest(method, uri, headers, body);
         return await _client.SendAsync(request);
     }
 
